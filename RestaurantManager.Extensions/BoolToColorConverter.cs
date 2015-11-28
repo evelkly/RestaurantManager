e56fd1b8cdfaf331143cf71a4add60f7b81857e3
evelkly <e.a.velkly@thermacore.com>
2015-11-28 00:05:55 -0500

    Module 3 Homework

---
+++ RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool) value ? Colors.Red : Colors.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Microsoft.Xaml.Interactivity;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior: DependencyObject, IBehavior
+    {
+        public void Attach(DependencyObject associatedObject)
+        {
+            this.AssociatedObject = associatedObject;
+            if (this.AssociatedObject is Page)
+            {
+                (this.AssociatedObject as Page).RightTapped += WarningDialog_RightTapped;
+            }
+        }
+
+        private async void WarningDialog_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog("Thank you for trying the demo.", "Thank you").ShowAsync();
+        }
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Page).RightTapped -= WarningDialog_RightTapped;
+        }
+
+        public DependencyObject AssociatedObject { get; private set; }
+    }
+}
